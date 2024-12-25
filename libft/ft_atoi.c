d19ed237e3868f11e4e8530ddc038177e1b45f76
rd01f <you@example.com>
2024-12-25 03:37:45 -0500

    fix atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -4,28 +4,23 @@ int ft_atoi(const char *nptr)
 {
     int i;
     int digit;
-    int len_digit;
-    int power;
-
+    int sign;
+    
     i = 0;
+    sign = 1;
     digit = 0;
-    power = 1;
-    len_digit = 0;
     while(ft_isspace((int)*(nptr + i)) == 1)
         i++;
     if(*(nptr + i) == '-' || *(nptr + i) == '+')
+    {
+        if(*(nptr + i) == '-')
+            sign = -1; 
         i++;
-    while(ft_isdigit((int)*(nptr + i + len_digit)) == 1)
+    }    
+    while(ft_isdigit((int)*(nptr + i)) == 1)
     {
-        len_digit++;
-        if(len_digit > 9)
-            return(INT_MAX);
+        digit = digit * 10 + ((int)*(nptr + i) - '0') ;
+        i++;
     }
-    len_digit--;
-    while(ft_isdigit((int)*(nptr + i + len_digit)) == 1)
-    {
-        digit += ((int)*(nptr + i + len_digit--) - '0') * power ;
-        power *= 10;
-    }
-    return(digit);
+    return(digit * sign);
 }
